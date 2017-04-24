e94ae0a8249934507705be6312414a90b357fd97
Norbert Kurz <info@nufuk.com>
2017-04-24 20:59:33 +0200

    alle ZeroMQ Examples

--- zeromq/zmq-filer-req-loop.js
+++ zeromq/zmq-filer-req-loop.js
@@ -7,16 +7,26 @@ const
   requester = zmq.socket('req');
 
 //handle replies from responder
+let lastTimestamp = Date.now();
 requester.on('message', function(data) {
   let response = JSON.parse(data);
   console.log("received response:", response);
+  lastTimestamp = response.timestamp;
 });
 
 requester.connect("tcp://localhost:5433");
 //send request for content
-for(let i=1; i<=3; i++) {
+for(let i=1; i<=30000; i++) {
   console.log("Sending request for: " + filename);
   requester.send(JSON.stringify({
     path: filename
   }));
 }
+let sendingEndTime = Date.now();
+//close the responder when the Node process ends
+process.on('SIGINT', function(){
+  console.log("End with sending: " + sendingEndTime);
+  console.log("So it took about: " + ((lastTimestamp - sendingEndTime)/1000) + " seconds");
+  console.log("Shutting down...");
+  requester.close();
+});
